ac9df0309faec9889e20fb112102b32ec4ca78e1
OleksandrBabilia <djesssash1708@gmail.com>
2023-07-26 22:02:23 +0300

    Updated: Changed Perceptron File Name

--- Perceptron/graphs.py
+++ Perceptron/graphs.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
 
-from perceptron import Perceptron
+from algo import Perceptron
 
 
 class PerceptronGraph:
